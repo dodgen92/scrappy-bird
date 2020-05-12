c7f64c3210abc5d2853122bed86f224fd85b1deb
dodgen92 <58264866+dodgen92@users.noreply.github.com>
2020-05-11 23:08:54 -0400

    Initial cpp file, bird physx params

---
+++ scrappybird.cpp
@@ -0,0 +1,17 @@
+private:
+
+	float fBirdPosition = 0.0f;
+	float fBirdVelocity = 0.0f;
+	float fBirdAcceleration = 0.0f;
+	float fGravity = 100.0f;
+	float fLevelPosition = 0.0f;
+
+	float fSectionWidth;
+	list<int> listSection;
+
+	bool bHasCollided = false;
+	bool bResetGame = false;
+
+	int nAttemptCount = 0;
+	int nFlapCount = 0;
+	int nMaxFlapCount = 0;
